0ab5c169798716a9cfeba03e6872548a376e47d4
Sára Juhošová <s.juhosova@student.tudelft.nl>
2022-03-19 12:06:00 +0100

    Add experiment suite for Strings

--- src/main/java/sa/assertj/Main.java
+++ src/main/java/sa/assertj/Main.java
@@ -1,12 +1,14 @@
 package sa.assertj;
 
-import sa.assertj.maps.ContainsExactly;
+import sa.assertj.strings.IsBase64;
+import sa.assertj.strings.IsBetween;
+import sa.assertj.strings.IsStrictlyBetween;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        Experiment exp = new ContainsExactly();
+        Experiment exp = new IsBase64();
         exp.run(100000, 100);
         try {
             exp.writeResults("out.csv");
