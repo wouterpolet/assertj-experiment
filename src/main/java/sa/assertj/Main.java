a520b79e81f6997794bb438b14a57dc4d4dd3150
Sára Juhošová <s.juhosova@student.tudelft.nl>
2022-03-21 19:00:35 +0100

    Add priting to experiment

--- src/main/java/sa/assertj/Main.java
+++ src/main/java/sa/assertj/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         Experiment exp = new ContainsExactly();
         exp.run(100000, 100);
+        exp.printOverview();
         try {
             exp.writeResults("out.csv");
         } catch (IOException e) {
